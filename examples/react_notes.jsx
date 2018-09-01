a00611dc3ed00c27ef63fc3e1530532137e4622d
jdlawler <joellawler@gmail.com>
2018-09-01 13:39:07 -0500

    Rendering Lists

--- examples/react_notes.jsx
+++ examples/react_notes.jsx
@@ -108,3 +108,39 @@ class App extends Component {
     return count === 0 ? <h1>Zero</h1> : count;
   }
 }
+
+// https://codewithmosh.com/courses/357787/lectures/5634610
+// Rendering Lists
+// *** Each child in an array or iterator should have a unique key
+
+class App extends Component {
+  state = {
+    count: 0,
+    tags: ["tag1", "tag2", "tag3"]
+  };
+
+  render() {
+    return (
+      <React.Fragment>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button className="btn btn-secondary btn-sm">Increment</button>
+        <ul>
+          {this.state.tags.map(tag => (
+            <li kay={tag}>{tag}</li>
+          ))}
+        </ul>
+      </React.Fragment>
+    );
+  }
+
+  getBadgeClasses() {
+    let classes = "bade m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? <h1>Zero</h1> : count;
+  }
+}
