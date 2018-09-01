f91fc0b5851083f76dcfc73d843453e3c523f58e
jdlawler <joellawler@gmail.com>
2018-09-01 13:31:26 -0500

    Rendering Classes Dynamically

--- examples/react_notes.jsx
+++ examples/react_notes.jsx
@@ -79,3 +79,32 @@ class App extends Component {
     return count === 0 ? <h1>Zero</h1> : count;
   }
 }
+
+// https://codewithmosh.com/courses/357787/lectures/5634625
+// Rendering Classes Dynamically
+
+class App extends Component {
+  state = {
+    count: 0
+  };
+
+  render() {
+    return (
+      <React.Fragment>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button className="btn btn-secondary btn-sm">Increment</button>
+      </React.Fragment>
+    );
+  }
+
+  getBadgeClasses() {
+    let classes = "bade m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? <h1>Zero</h1> : count;
+  }
+}
