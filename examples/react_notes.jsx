8b2bad55cdda90293ee3d1fdd1bc07c48f87d4ee
jdlawler <joellawler@gmail.com>
2018-09-01 22:11:35 -0500

    What happens when state changes

--- examples/react_notes.jsx
+++ examples/react_notes.jsx
@@ -340,3 +340,44 @@ class App extends Component {
     }
   }
   export default App 
+
+
+// https://codewithmosh.com/courses/357787/lectures/5634611
+// What happens when state changes
+// The virtual dom is compared to the old dom and only the element
+// that has been changed is effected
+
+
+// https://codewithmosh.com/courses/357787/lectures/5634618
+// Passing Event Arguments
+
+class App extends Component {
+    state = {
+      count: 0
+    };
+  
+    handleIncrement = () => {
+       this.setState({ count: this.state.count + 1 });
+    }
+
+    render() {
+      return (
+        <React.Fragment>
+          <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+          <button onClick={this.handleIncrement} className="btn btn-secondary btn-sm">Increment</button>
+        </React.Fragment>
+      );
+    }
+  
+    getBadgeClasses() {
+      let classes = "bade m-2 badge-";
+      classes += this.state.count === 0 ? "warning" : "primary";
+      return classes;
+    }
+  
+    formatCount() {
+      const { count } = this.state;
+      return count === 0 ? <h1>Zero</h1> : count;
+    }
+  }
+  export default App 
