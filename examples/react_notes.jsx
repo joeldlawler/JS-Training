7079b50f9d17f2690ad21a26b778865a3514bc32
jdlawler <joellawler@gmail.com>
2018-09-05 09:48:19 -0500

    Destructuring Arguments

--- examples/react_notes.jsx
+++ examples/react_notes.jsx
@@ -594,3 +594,51 @@ const NavBar = props => {
 };
 
 export default NavBar;
+
+// Destructuring Arguments
+// https://codewithmosh.com/courses/357787/lectures/5634724
+
+const NavBar = ({ totalCounters }) => {
+    return (
+      <nav className="navbar navbar-light bg-light">
+        <a className="navbar-brand" href="#">
+          Navbar{" "}
+          <span className="badge badge-pill badge-secondary">
+            {totalCounters}
+          </span>
+        </a>
+      </nav>
+    );
+  };
+  
+  export default NavBar;
+
+
+// this.props is Destructured to const making the code cleaner
+
+import React, { Component } from "react";
+import Counter from "./counter";
+
+class Counters extends Component {
+  render() {
+    const { onReset, counters, onDelete, onIncrement } = this.props;
+
+    return (
+      <div>
+        <button onClick={onReset} className="btn btn-primary btn-sm m-2">
+          Reset
+        </button>
+        {counters.map(counter => (
+          <Counter
+            key={counter.id}
+            onDelete={onDelete}
+            onIncrement={onIncrement}
+            counter={counter}
+          />
+        ))}
+      </div>
+    );
+  }
+}
+
+export default Counters;
