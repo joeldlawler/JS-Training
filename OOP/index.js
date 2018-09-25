ca6c49a60721704c42ee1e51608a4ed15fefb8c9
jdlawler <joellawler@gmail.com>
2018-09-25 13:17:09 -0500

    Modularity

--- OOP/index.js
+++ OOP/index.js
@@ -1,33 +1,5 @@
-const _items = new WeakMap();
-
-class Stack {
-  constructor() {
-    _items.set(this, []);
-  }
-
-  push(obj) {
-    _items.get(this.push(obj));
-  }
-
-  pop() {
-    const items = _items.get(this);
-
-    if (items.length === 0) throw new Error("Empty Stack");
-    return items.pop();
-  }
-
-  peak() {
-    const items = _items.get(this);
-
-    if (items.length === 0) throw new Error("Empty Stack");
-
-    return items[items.length - 1];
-  }
-
-  get count() {
-    const items = _items.get(this);
-    return items.length;
-  }
-}
-
-const s = new Stack();
+// Modules
+//  Benefits
+//    Maintainability
+//    Reuse
+//    Abstract
